77bdcafc5a300dc2e8fc951d7bde16c32231adeb
Artur Valdna <arturvaldna@gmail.com>
2022-01-03 08:38:45 +0200

    Added enums for InputData

---
+++ Roulette/Models/ColumnEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace Roulette.Models
+{
+    /// <summary>
+    /// ColumnEnum contains 3 columns with names: 
+    ///     A - The first
+    ///     B - The second
+    ///     C - The Third 
+    /// </summary>
+    public enum ColumnEnum
+    {
+        C = 0,
+        A = 1,
+        B = 2,
+        WithoutColumn = 99
+    }
+}
---
+++ Roulette/Models/DozenEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Roulette.Models
+{
+    /// <summary>
+    /// DozenEnum contains 3 numbers for showing a number of dozen.
+    /// </summary>
+    public enum DozenEnum
+    {
+        First = 1,
+        Second = 2,
+        Third = 3,
+        WithoutDozen = 99
+    }
+}
