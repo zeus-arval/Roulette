6405fc7db71bcd5d96cfec28ce408784bf8d06c2
Artur Valdna <arturvaldna@gmail.com>
2022-01-05 13:55:12 +0200

    Added NumberBlockDTO for holding data for TextBlock and Rectangle for a certain number

---
+++ Roulette/UIElements/NumberBlockDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette.UIElements
+{
+    /// <summary>
+    /// Class for holding data for TextBlock and Rectangle elements for a certain number
+    /// </summary>
+    public class NumberBlockDTO
+    {
+        public int GridColumn { get; set; }
+        public int GridRow { get; set; }
+        public string RectangleName { get; set; }
+        public string TextBlockName { get; set; }
+        public string Fill { get; set; }
+        public string Foreground { get; set; }
+        public string Text { get; set; }
+    }
+}
